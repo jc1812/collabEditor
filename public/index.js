6643f09c4c47e8655ade5c4d9cf9853b18bd262e
Jordan Craig <jordancraig1812@gmail.com>
2020-04-17 11:43:48 +0800

    Add CRDT locally

--- public/index.js
+++ public/index.js
@@ -42,3 +42,72 @@ function printMessage(messageText, userColor, userName) {
     message.appendChild(text);
     chat.appendChild(message);
 }
+
+// This class holds our editors data structure. It stores the CRDT chars,
+// recieves them from across the network and inserts them into ace editor.
+class CRDT {
+    constructor(id) {
+        this.siteId = id;
+        this.struct = []; //Stores the characters
+    }
+
+    localInsert(value, index) {
+        const char = this.generateChar(value, index);
+        this.struct.splice(index, 0, char);
+        console.log(this.struct);
+        return char;
+    }
+
+    generateChar(char, index) {
+        let before = this.struct[index-1] ?  this.struct[index-1].pos : 0;
+        let pos;
+
+        if(this.struct[index]) {
+            let after = this.struct[index].pos;
+            pos = before + ((after - before)/2);
+        } else {
+            let after = 1;
+            pos = Math.floor(before)+after;
+        }
+
+        let newChar = new CRDTChar(char, pos, this.siteId);
+
+        return newChar;
+    }
+
+    localDelete(index) {
+        let char = this.struct.splice(index, 1)[0];
+        console.log(this.struct);
+        return char;
+    }
+}
+
+class CRDTChar {
+    constructor(char, pos, siteId) {
+        this.pos = pos; // Number between 0 and 1
+        this.char = char;
+        this.siteId = siteId;
+    }
+}
+
+let crdt = new CRDT(1);
+
+// This function translates editor input into our CRDT data structure.
+// It also sends the instruction across the network.
+editor.session.on('change', (e) => {
+    if(editor.curOp && editor.curOp.command.name) {
+        let lines = editor.session.getLines(0, editor.session.getLength());
+        let length = 0;
+        for(let i=0; i<e.start.row; i++) {
+            length += lines[i].length;
+            if(i < lines.length-1) length += 1; //for new line character
+        }
+        length += e.start.column; 
+        if(e.action === "insert") {
+            let char = crdt.localInsert(e.lines.join("\n"), length);
+        }
+        if(e.action === "remove") {
+            let char = crdt.localDelete(length);
+        }
+    }
+});
