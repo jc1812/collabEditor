1dfbe633c6df293fd430d7e120b104a980a8e228
Jordan Craig <jordancraig1812@gmail.com>
2020-04-17 10:05:45 +0800

    Initial commit
    Add front end ui and basic express server.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(port, console.log(`App running on port ${port}`));
