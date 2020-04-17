48296eda80fb631d8b0fe750e458663041e7ef95
Jordan Craig <jordancraig1812@gmail.com>
2020-04-17 11:10:12 +0800

    Add local chat

--- public/index.js
+++ public/index.js
@@ -3,3 +3,36 @@ editor.setTheme("ace/theme/dracula");
 editor.session.setMode("ace/mode/javascript");
 
 let socket = io();
+
+let name = document.getElementById('name');
+let color = document.getElementById('chatColor');
+let msg = document.getElementById('msg');
+let send = document.getElementById('send');
+let chat = document.getElementById('chat');
+
+send.addEventListener('click', (e) => {
+    e.preventDefault();
+    let messageText = msg.value;
+    let userName = name.value;
+
+    printMessage(messageText, color.value, userName);
+
+    chat.scrollTop = chat.scrollHeight;
+
+    msg.value = "";
+});
+
+function printMessage(messageText, userColor, userName) {
+    let message = document.createElement('p');
+    let text = document.createTextNode(`${messageText}`);
+
+    // Create a span for the users handle
+    let handle = document.createElement('span');
+    handle.setAttribute('style', `color: ${userColor}`);
+    let handleText = document.createTextNode(`${userName}:  `);
+    handle.appendChild(handleText);
+
+    message.appendChild(handle);
+    message.appendChild(text);
+    chat.appendChild(message);
+}
